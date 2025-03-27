b7590756067efb6a8f94859c7bbebdeb248dc350
Caziggy <tazzapia@gmail.com>
2025-03-27 10:49:51 -0600

    Made goal reference the root for camera object, but the method of calling the text label is very fragile, will break if camera children are reorganized in the future.

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -2,6 +2,10 @@ class_name Goal
 extends Node2D
 static var totalCount = 0
 static var finalTotalCount = 0
+
+@onready var cam = get_node("/root/Main/Camera2D")
+
+
 func _init() -> void:
 	totalCount += 1
 	print(totalCount)
@@ -9,8 +13,11 @@ func _init() -> void:
 	
 func _ready() -> void:
 	finalTotalCount = totalCount
-	$"../../../../Camera2D/RichTextLabel".text = str(totalCount) + "/" + str(finalTotalCount)
-
+	#$"../../../../Camera2D/RichTextLabel".text = str(totalCount) + "/" + str(finalTotalCount)
+	
+	
+	#TODO: This is very fragile. If I reorganize camera this breaks.
+	cam.get_child(4).text = str(totalCount) + "/" + str(finalTotalCount)
 
 
 func _on_goal_area_area_entered(area: Area2D) -> void:
@@ -18,4 +25,5 @@ func _on_goal_area_area_entered(area: Area2D) -> void:
 		totalCount -= 1
 		$GoalArea/GoalColl.set_deferred("disabled", true)
 		$GoalArea.visible = false
-		$"../../../../Camera2D/RichTextLabel".text = str(totalCount) + "/" + str(finalTotalCount)
+		#$"../../../../Camera2D/RichTextLabel".text = str(totalCount) + "/" + str(finalTotalCount)
+		cam.get_child(4).text = str(totalCount) + "/" + str(finalTotalCount)
